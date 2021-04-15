265f2fb2a11e436f4fa220cc6c1038a752e38982
dw <devdw98@gmail.com>
2021-04-15 15:11:43 +0900

    글 id기능 추가

--- src/dao/counter.ts
+++ src/dao/counter.ts
@@ -1,9 +1,12 @@
-import { db } from '../app';
-import { Collection } from 'mongodb';
+import { dbAdmin } from '../app';
+import { Db, Collection } from 'mongodb';
 
 const counterCollection = `counters`;
 
-export async function getNextSequence(collectionName: string): Promise<number> {
+export async function getNextSequence(
+  db: Promise<Db>,
+  collectionName: string
+): Promise<number> {
   const collection = (await db).collection(counterCollection);
   const doc = await collection.findOneAndUpdate(
     { _id: collectionName },
